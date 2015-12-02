28859efad8e9c62f45e0ec4717e57e6590011bd2
Takeshi Kawai <takezou621@gmail.com>
2015-12-03 00:14:04 +0900

    initial commit

---
+++ hellogreenhouse/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hellogreenhouse
+//
+//  Created by Takeshi Kawai on 2015/12/03.
+//  Copyright © 2015年 Takeshi Kawai. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
